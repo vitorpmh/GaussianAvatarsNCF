e642b851468082f8aca1aa42d481d25b3d3e6453
vitor-pc-casa <vitorpmh@gmail.com>
2025-04-29 19:37:22 -0300

    [ENHANCEMENT] Implement real-time morphing and blending in local viewer

--- save_processed_gaussians.py
+++ save_processed_gaussians.py
@@ -27,37 +27,38 @@ from gaussian_renderer import render
 
 @dataclass
 class Config(Mini3DViewerConfig):
-    point_path: Optional[Path] = None
-    """Path to the gaussian splatting file"""
+    gaussians_path: Optional[Path] = None
+    """Path to folder containing multiple id/point_cloud.ply and id/flame_param.npz"""
     sh_degree: int = 3
     """Spherical Harmonics degree"""
     save_folder: Path = Path("./viewer_output")
 
 def process_gaussians(cfg):
-    if (Path(cfg.point_path).parent / "flame_param.npz").exists():
-        gaussians = FlameGaussianModel(cfg.sh_degree)
-    else:
-        gaussians = GaussianModel(cfg.sh_degree)
-
-    unselected_fid = []
-        
-        
-    if cfg.point_path is not None:
-        if cfg.point_path.exists():
-                gaussians.load_ply(cfg.point_path, has_target=False, motion_path=None, disable_fid=unselected_fid)
+    for id_path in cfg.gaussians_path.iterdir():
+        if (id_path / "flame_param.npz").exists():
+            gaussians = FlameGaussianModel(cfg.sh_degree)
         else:
-            raise FileNotFoundError(f'{cfg.point_path} does not exist.')
-        
-    save_path = cfg.save_folder 
-        
-    if (Path(cfg.point_path).parent / "flame_param.npz").exists():
-        gaussians.save_ply_3dgs_format(f'{save_path}/point_cloud.ply')
-        np.save(f"{save_path}/lmk.npy",
-            gaussians.get_landmarks().squeeze(0).cpu().numpy())
-        np.save(f"{save_path}/mesh.npy",
-            gaussians.get_meshes().squeeze(0).cpu().numpy())
-        np.save(f"{save_path}/selected_lmk.npy",
-            gaussians.get_landmarks_from_meshes())
+            gaussians = GaussianModel(cfg.sh_degree)
+
+        unselected_fid = []
+            
+        ply_path = id_path / "point_cloud.ply"  
+        if ply_path is not None:
+            if ply_path.exists():
+                    gaussians.load_ply(ply_path, has_target=False, motion_path=None, disable_fid=unselected_fid)
+            else:
+                raise FileNotFoundError(f'{ply_path} does not exist.')
+            
+        save_path = cfg.save_folder / id_path.name
+            
+        if (id_path / "flame_param.npz").exists():
+            gaussians.save_ply_3dgs_format(f'{save_path}/point_cloud.ply')
+            np.save(f"{save_path}/lmk.npy",
+                gaussians.get_landmarks().squeeze(0).cpu().numpy())
+            np.save(f"{save_path}/mesh.npy",
+                gaussians.get_meshes().squeeze(0).cpu().numpy())
+            np.save(f"{save_path}/selected_lmk.npy",
+                gaussians.get_landmarks_from_meshes())
 
 
 if __name__ == "__main__":
